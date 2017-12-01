6895aa2424b203f01ed665e193aa880754fe6bb5
Marit Oliemans <Marit@Marits-MacBook-Pro.local>
2017-12-01 11:37:40 -0700

    Add colorPicker eventListener

--- designs.js
+++ designs.js
@@ -1,4 +1,10 @@
-// Select color input
+var color = "black";
+
+// listening to colorPicker change and setting its value to variable color
+$("#colorPicker").on("change", function(event){
+	color = event.target.value;
+})
+
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
