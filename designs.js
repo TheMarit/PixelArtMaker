f4f0d270c0cffcde7a4232856b77189e8d446e84
Marit Oliemans <Marit@Marits-MacBook-Pro.local>
2017-12-01 12:31:33 -0700

    Add Grid with eventlistener on submit button

--- designs.js
+++ designs.js
@@ -1,16 +1,28 @@
 var color = "black";
-
 // listening to colorPicker change and setting its value to variable color
 $("#colorPicker").on("change", function(event){
 	color = event.target.value;
 })
 
-// Select size input
+// Create grid when user click submit
+$("#sizePicker").submit(function(event){
+	event.preventDefault();
+	$("#pixel_canvas").html(makeGrid(event.target[0].value,event.target[1].value));
+});
 
-// When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
 
-// Your code goes here!
-
-}
+function makeGrid(rows, colums) {
+	var rowBeginning = "<tr>";
+	var rowEnding = "</tr>";
+	var tableData = "<td></td>";
+	var colum = "";
+	var grid = "";
+	for (var i = 0; i < colums; i++){
+		colum += tableData;
+	};
+	for (var j = 0; j< rows; j++){
+		grid = grid + rowBeginning + colum + rowEnding;
+	};
+	return grid;
+};
